3067bbf3831abe30c201a402ce2cad48e9f5763e
Orochiaziz <124267123+Orochiaziz@users.noreply.github.com>
2023-03-13 15:07:42 +0000

    Create ex03.php

---
+++ ex003/ex03.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Testes de tipos permitivos</title>
+</head>
+<body>
+    <h1>Testes de tipos Permitivos</h1>
+    <?php 
+        // 0x = EXADECIMAL 0B = binário 0 = Octal
+        $num = (is_float( 9.9));
+       print "O número da variavel é $num ";
+       var_dump($num)
+    ?>
+    <script>
+        // var num = 01010;
+      //  window.document.write(num)
+    </script>
+</body>
+</html>
